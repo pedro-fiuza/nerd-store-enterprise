6d4c4d62df92a68d82b875647b2325d3ce498576
pedro-fiuza <pdrfiuza@gmail.com>
2021-04-20 01:57:57 -0300

    Configuracao do Identity

--- src/Services/NSE.Identidade.API/Startup.cs
+++ src/Services/NSE.Identidade.API/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NSE.Identidade.API.Data;
 
 namespace NSE.Identidade.API
 {
@@ -26,12 +21,16 @@ namespace NSE.Identidade.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                    .AddRoles<IdentityRole>() //support of role
+                    .AddEntityFrameworkStores<ApplicationDbContext>() //source of data
+                    .AddDefaultTokenProviders(); //add token to reset password or something like that
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NSE.Identidade.API", Version = "v1" });
-            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +39,6 @@ namespace NSE.Identidade.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "NSE.Identidade.API v1"));
             }
 
             app.UseHttpsRedirection();
@@ -49,6 +46,7 @@ namespace NSE.Identidade.API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseEndpoints(endpoints =>
             {
