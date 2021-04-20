43e7d5754f35615d411cde1667394bce3cf44e03
pedro-fiuza <pdrfiuza@gmail.com>
2021-04-20 02:23:14 -0300

    Registro e Login de usuario

---
+++ src/Services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identidade.API.Models;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Controllers
+{
+    [Route("api/identity")]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("new-account")]
+        public async Task<IActionResult> Register(UserRegister userRegister)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = userRegister.Email,
+                Email = userRegister.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, userRegister.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("authentication")]
+        public async Task<IActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result =  await _signInManager.PasswordSignInAsync(userLogin.Email, userLogin.Password , false, true);
+
+            if (result.Succeeded) return Ok();
+
+            return BadRequest();
+        }
+    }
+}
---
+++ src/Services/NSE.Identidade.API/Models/UserViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Identidade.API.Models
+{
+    public class UserRegister
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [EmailAddress(ErrorMessage = "The field {0} is in invalid format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [StringLength(100, ErrorMessage = "The field {0} must has between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Senha", ErrorMessage = "The passwords doesn't match")]
+        public string PasswordConfirmation { get; set; }
+    }
+
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [EmailAddress(ErrorMessage = "The field {0} is in invalid format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        [StringLength(100, ErrorMessage = "The field {0} must has between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+
+}
