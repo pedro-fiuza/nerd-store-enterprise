7a3124e7acf06a9d88ea10fb3c94b0703f376a1a
pedro-fiuza <pdrfiuza@gmail.com>
2021-08-27 04:41:20 -0300

    Configurando Autenticacao

--- src/Web/NSE.WebApp.MVC/Startup.cs
+++ src/Web/NSE.WebApp.MVC/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.WebApp.MVC.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,33 +25,13 @@ namespace NSE.WebApp.MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddIdentityConfiguration();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
+            app.UseWebApiConfig(env);
         }
     }
 }
