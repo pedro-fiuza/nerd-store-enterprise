badb12f4bae14360e6bdce4e5c9ed81f27692f24
pedro-fiuza <pdrfiuza@gmail.com>
2021-08-27 06:11:34 -0300

    Gerando cookie atraves do JWT e Obtendo dados dele

--- src/Web/NSE.WebApp.MVC/Startup.cs
+++ src/Web/NSE.WebApp.MVC/Startup.cs
@@ -24,8 +24,11 @@ namespace NSE.WebApp.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
             services.AddIdentityConfiguration();
+
+            services.AddRazorPages();
+
+            services.RegisterServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
