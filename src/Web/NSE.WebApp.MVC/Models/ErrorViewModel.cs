8172d0b5664dc1c67caefbca77e2663dd1fecda0
pedro-fiuza <pdrfiuza@gmail.com>
2021-08-27 06:00:52 -0300

    Consumindo Api via HTTPService e Padronizando retorno

---
+++ src/Web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Models
+{
+
+    public class ErrorViewModel
+    {
+        public int ErroCode { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
+    }
+
+    public class ResponseResult
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public ResponseErrorMessages Errors { get; set; }
+    }
+
+    public class ResponseErrorMessages
+    {
+        public List<string> Mensagens { get; set; }
+    }
+
+}
