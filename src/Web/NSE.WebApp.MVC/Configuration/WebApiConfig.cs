cfe15524a9dd2b1d60310ccedd5ef7aedbe82e27
pedro-fiuza <pdrfiuza@gmail.com>
2021-08-27 07:19:28 -0300

    Pegando AppSettings

--- src/Web/NSE.WebApp.MVC/Configuration/WebApiConfig.cs
+++ src/Web/NSE.WebApp.MVC/Configuration/WebApiConfig.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.WebApp.MVC.Extensions;
 
@@ -7,6 +9,10 @@ namespace NSE.WebApp.MVC.Configuration
 {
     public static class WebApiConfig
     {
+        public static void AddMvcConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<AppSettings>(configuration);
+        }
 
         public static void UseWebApiConfig(this IApplicationBuilder app, IWebHostEnvironment env)
         {
